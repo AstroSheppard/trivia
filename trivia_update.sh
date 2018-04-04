924b95e006b726d95dc89dd34ccfdc09e3ce9050
AstroSheppard <kshep23@umd.edu>
2018-04-04 11:53:12 -0400

    Add files via upload

--- trivia_update.sh
+++ trivia_update.sh
@@ -9,4 +9,4 @@ python2 update_schedule.py md
 sleep 1m
 python2 update_stats.py s md
 sleep 1m
-python2 get_questions.py s next_md
+python2 retrieve_questions.py s next_md
--- update_stats.py
+++ update_stats.py
@@ -1,16 +1,12 @@
-import pygsheets
 import sys
+
+import pygsheets
 import pandas as pd
 import numpy as np
 
+from get_functs import access
 
-def access():
-    json='trivia-727e12dbc64c.json'
-    ### Get credentials/ authorize
-    gc=pygsheets.authorize(service_file='trivia-727e12dbc64c.json')
-    ### Read in entire sheet
-    sheet = gc.open("Trivia")
-    return sheet
+
 def clean_data(data,s, md, players):
     data=data[(data['Season']==int(s)) & (data['Match Day']==int(md))]
     labels=['Season','Match Day', 'Unnamed: 0','answers']
@@ -20,29 +16,30 @@ def clean_data(data,s, md, players):
     data=data.drop(labels, axis=1)
     return data
 
-s=sys.argv[1]
-md=sys.argv[2]
-sheet=access()
-wks=sheet.worksheet_by_title('Stats')
+if __name__ == "__main__":
+    s=sys.argv[1]
+    md=sys.argv[2]
+    sheet=access()
+    wks=sheet.worksheet_by_title('Stats')
 
 
-# read in stats dataframe
-stats=wks.get_as_df(start='A2', end='Q20', index_colum=1)
-players=wks.get_as_df(start='A1', end='S1')
-players= filter(None, players)
-data=clean_data(pd.read_csv('all_data.csv'),s,md, players)
-pc=[item+'_correct' for item in players]
-pr=[item+' Right' for item in players]
-pw=[item+' Wrong' for item in players]
+    # read in stats dataframe
+    stats=wks.get_as_df(start='A2', end='Q20', index_colum=1)
+    players=wks.get_as_df(start='A1', end='S1')
+    players= filter(None, players)
+    data=clean_data(pd.read_csv('all_data.csv'),s,md, players)
+    pc=[item+'_correct' for item in players]
+    pr=[item+' Right' for item in players]
+    pw=[item+' Wrong' for item in players]
 
-for i, cat in enumerate(data['categories'].values):
-    sub=data.iloc[i,:-1]
-    for j,name in enumerate(pc):
-        if sub.loc[name]:
-            stats.loc[cat.strip(),pr[j]]+=1
-        else:
-            stats.loc[cat.strip(),pw[j]]+=1
+    for i, cat in enumerate(data['categories'].values):
+        sub=data.iloc[i,:-1]
+        for j,name in enumerate(pc):
+            if sub.loc[name]:
+                stats.loc[cat.strip(),pr[j]]+=1
+            else:
+                stats.loc[cat.strip(),pw[j]]+=1
 
-wks.set_dataframe(stats, 'B2')
+    wks.set_dataframe(stats, 'B2')
 
 
